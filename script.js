00a3753f0f9a6d5c1ca2e2fd4b8ae98faf471038
42plat0 <gytisnaujasis@icloud.com>
2024-01-06 20:30:06 +0200

    update game to best of five
    add best of five game, include proper name of choice, congratulate/!congratulate message

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+//variables for capitalizing choices of players
+let playerName, computerName;
+
 //Get human input
 //store input as lowercase 
 function playerSelection(){
     const playerInput = prompt("Rock, Paper or Scissors?");
-    return playerInput.toLowerCase();
+    const playerInputInput = playerInput.toLowerCase();
+    playerName = playerInput.toLowerCase();
+    return playerInputInput;
 }
 //generate computer choice
 function computerSelection(){
     const arrayOfChoices = ["rock", "paper", "scissors"];
     const randomNumber = Math.floor(Math.random() * 3);
-    return arrayOfChoices[randomNumber];
+    computerName = arrayOfChoices[randomNumber];
+    return computerName;
 }
 //play round
 function playRound(playerSelection, computerSelection){
@@ -54,23 +60,68 @@ function playRound(playerSelection, computerSelection){
     }
 //when its a ties print 'its a tie!'
     if (isTie){
-        return "It's a tie!"
+        return null;
     }
 //if its not a tie, print who won
     else{
         if (playerWin){
-            return `You win! ${playerSelection} beats ${computerSelection}!`
+            
+            return true;
         }
         else{
-            return `You lose... ${playerSelection} loses to ${computerSelection}.`
+            return false;
         }
     }
 }
+//play five rounds of game
+//reiskias, kad reikes pasalinti paskutines eilutes, kurios paskelbia nugaletoja, o tiesog funkcijos pabaigoje grazinti rezultata
+    //if (isTie) grazinti, kad buvo lygiosios
+    //kitu atveju, grazinti nugaletoja
+        //jei playerWin yra true, zmogus laimejo
+        //else kompas
+//reikia prideti best of five logika
+    //playround logika grazina nugaletoja
+    //jei kompiuteris laimejo - kompiuteriui taskas
+    //jei zmogus laimejo - zmogui taskas
+    //daugiausiai tasku surinkes laimi
 
 function game(){
-    for (let i = 0; i < 5; i++){
-        console.log(playRound(playerSelection(), computerSelection()));
+    //keep score of player and computer
+    let playerScore = 0;
+    let computerScore = 0;
+    //proper names to display in the client
+    let playerChoice;
+    let computerChoice;
+    //condition for a while loop
+    let keepGoing = true;
+
+    //algorith for best of five
+    while (keepGoing){
+        // when computer wins, add points to computer
+        if(playRound(playerSelection(), computerSelection()) == false){
+            computerScore++;
+        //when player wins - to player
+        }else{
+            playerScore++;
+        }
+        //makes proper name of the choice of a player with capitalized letter 
+        playerChoice = playerName[0].toUpperCase() + playerName.slice(1);
+        computerChoice = computerName[0].toUpperCase() + computerName.slice(1);
+        //score logger
+        console.log(`${playerScore} : ${computerScore}, Your ${playerChoice} beats ${computerChoice}`)
+        //when one of the players reaches 3 points, the game ends.
+        if(computerScore === 3 || playerScore === 3){
+            keepGoing = false;
+        }
     }
+        if (playerScore > computerScore){
+            console.log("You win!\nCongratulations!")
+        }
+        else{
+            console.log("Computer bested you! \nBetter luck next time!")
+        }
 }
 
-game();
+//play the game
+console.log(game());
+
